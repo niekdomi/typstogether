41debda99a9fac85b3d6be2e0f146bf064bbb2a2
domi <152265924+niekdomi@users.noreply.github.com>
2026-04-27 10:43:12 +0200

    Use bun native sql bindings
    - One depdency less
    
    - Maybe better ergonomics
    
    - In theory faster though likely no effect in real world scenario

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,15 +1,13 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/bun-sql";
+import { SQL } from "bun";
 import * as schema from "./schema";
 
 const databaseUrl =
   process.env["DATABASE_URL"] ?? "postgres://postgres:postgres@localhost:5432/typstogether";
 
-export const client = postgres(databaseUrl, {
-  max: Number(process.env["DATABASE_POOL_SIZE"] ?? 10),
-});
+export const client = new SQL(databaseUrl);
 
-export const db = drizzle(client, { schema });
+export const db = drizzle({ client, schema });
 
 export type Db = typeof db;
 export { schema };
