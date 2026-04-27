7734972b24252aeda5d72c274ef699ce092b234d
domi <152265924+niekdomi@users.noreply.github.com>
2026-04-27 11:32:25 +0200

    Uncomment workflows

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL ?? "/",
+  base: process.env["VITE_BASE_URL"] ?? "/",
   plugins: [svelte(), wasm()],
   build: {
     target: "esnext",
