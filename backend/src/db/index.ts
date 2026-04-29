0428c312444478a5a254422c415626b3194280df
David Yves Bachmann <david.yves.bachmann@gmail.com>
2026-04-29 14:23:11 +0200

    refactor: remove unnecessary export

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,10 +4,8 @@ import { drizzle } from "drizzle-orm/bun-sql";
 import { databaseUrl } from "../env";
 import * as schema from "./schema";
 
-export const client = new SQL(databaseUrl);
+const client = new SQL(databaseUrl);
 
 export const db = drizzle({ client, schema });
 
 export type Db = typeof db;
-// eslint-disable-next-line unicorn/prefer-export-from
-export { schema };
